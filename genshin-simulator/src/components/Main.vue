dd928cff96f3e3f2559dcd21354cc0966c7a69ff
Hololm <jholm6332@dysartstudents.org>
2022-04-23 16:26:59 -0700

    routing buttons

--- genshin-simulator/src/components/Main.vue
+++ genshin-simulator/src/components/Main.vue
@@ -11,17 +11,17 @@
     </el-select>
   </div>
   <div class="bannerImage">
-    <img v-bind:src="'../gacha-banners/charbanner/' + value + '.png'" :alt="value" style="padding: 10px">
+    <img v-bind:src="'../gacha-banners/charbanner/' + value + '.png'" :alt="value">
     <div class="details">
       <el-row>
         <el-button round plain color="#f2b738" @click="$router.push('/details')">Details</el-button>
-        <el-button round plain color="#f2b738" @click="">Inventory</el-button>
+        <el-button round plain color="#f2b738" @click="$router.push('/inventory')">Inventory</el-button>
       </el-row>
     </div>
   </div>
   <el-row class="wish">
-    <el-button round plain color="#f2b738" @click="">Wish (x1)</el-button>
-    <el-button round plain color="#f2b738" @click="">Wish (x10)</el-button>
+    <el-button round plain color="#f2b738" @click="$router.push('/wish1')">Wish (x1)</el-button>
+    <el-button round plain color="#f2b738" @click="$router.push('/wish10')">Wish (x10)</el-button>
   </el-row>
   </body>
 </template>
