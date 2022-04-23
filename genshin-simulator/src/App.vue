b63ef1edccde0be0ac7710ae6577497d69bd99b5
Hololm <jholm6332@dysartstudents.org>
2022-04-22 22:46:47 -0700

    button to change banner name

--- genshin-simulator/src/App.vue
+++ genshin-simulator/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <body>
   <div>
-    <div>
-      <button @click="count++">{{ count }}</button>
+    <div class="bannerName">
+      <el-button @click="bannerChange()">{{ banner[counter] }}</el-button>
     </div>
   </div>
   </body>
@@ -13,14 +13,20 @@ export default {
   name: "App",
   data() {
     return {
-      count: 0
+      banner: ['Hu-Tao', 'Ganyu', 'Venti', 'Kamisato Ayato', 'Kamisato Ayaka', 'Sangonomiya Kokomi', 'Kaedehara Kazuha', 'Raiden Shogun'],
+      counter: 0,
     }
   },
   methods: {
-
-  }
+    bannerChange() {
+      this.counter++
+      if (this.counter >= this.banner.length) {
+        this.counter = 0;
+      }
+      return this.banner[this.counter];
+    }
+      },
 }
 </script>
 <style>
-
 </style>
